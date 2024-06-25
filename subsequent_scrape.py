5cbde81677e03d84f7a4c5d2152a833b4648d05d
Liam Pearson <pearson.liam@gmail.com>
2024-06-25 18:07:44 +1000

    data is now pushed to awsS3

--- subsequent_scrape.py
+++ subsequent_scrape.py
@@ -2,42 +2,87 @@
 # coding: utf-8
 
 #! pip install html-table-parser-python3
-
-import pandas as pd
 import utils
 
+import pandas as pd
+import numpy as np
+import boto3
+import os
+import io
+from dotenv import load_dotenv #for loading env variables
+
 print("BEGIN SCRIPT")
+print("1. Download existing dataset from S3..")
+
+# Load the environment variables from .env
+load_dotenv()
+
+#get aws keys
+CLIENT_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+CLIENT_SECRET = os.environ.get("AWS_SECRET_ACCESS_KEY") 
+
+session = boto3.Session(
+    aws_access_key_id=CLIENT_ID,
+    aws_secret_access_key=CLIENT_SECRET)
+
+# Create an S3 client using the boto3 session above
+s3 = session.client('s3')
+
+# Replace with your bucket name and object key
+bucket_name = 'nsw-food-authority-name-and-shame'
+object_key = 'dataset.csv'
+
+# Retrieve object from S3
+obj = s3.get_object(Bucket=bucket_name, Key=object_key)
+
+# Read the object's content into a Pandas DataFrame
+prev_df = pd.read_csv(io.BytesIO(obj['Body'].read()))
+print("   Complete. Shape: {}\n".format(prev_df.shape))
+
+print("2. Get all notices currently on the foodauthority website...")
 #the parent page we are going to scrape
 url = "https://www.foodauthority.nsw.gov.au/offences/penalty-notices"#?page=15
-print("scraping data from: {}\n".format(url))
+print("   scraping data from: {}".format(url))
 
-#scrape each page and get the table
+#scrape each of the pages and get the table of notices
 notice_df = utils.scrape_tables(url)
-    
-notice_df.head() #inspect dataframe
 
 
-#Determine the new notice_numbers
 
 #get current dataset
-dataset = "nsw_food_auth_name_and_shame.csv"
-print("reading in dataset...")
-prev_df = pd.read_csv(dataset)
-print("Complete; shape: {}\n".format(prev_df.shape))
-      
-prev_df['notice_number'] = prev_df['notice_number'].astype(str) #convert column to string
+#filename = "nsw_food_auth_name_and_shame.csv"
+#print("reading in dataset...")
+#prev_df = pd.read_csv(filename)
 
+#Determine the new notice_numbers
+prev_df['notice_number'] = prev_df['notice_number'].astype(str) #convert column to string
+prev_df['date_removed_from_website']='' #once off DELETE
+
+print("3. Comparing website to dataset...")
 #compare the previous notice_numbers to the current ones
 old_notice_numbers = prev_df['notice_number'].tolist()
 current_notice_numbers = notice_df['notice_number'].tolist()
+print(type(old_notice_numbers[0])) #dev - unsure why date-removed is not being updated
+print(type(current_notice_numbers[0])) #dev - unsure why date-removed is not being updated
+
 new_notice_numbers = set(current_notice_numbers) - set(old_notice_numbers)
+removed_notice_numbers = set(old_notice_numbers) - set(current_notice_numbers)
+
+#check if any removed notice numbers
+if len(removed_notice_numbers)==0:
+    print("   no notice_numbers removed")
+    
+else:
+    print("   {} notice_numbers removed".format(len(removed_notice_numbers)))
+    prev_df = utils.handle_removed_notices(prev_df, removed_notice_numbers)
 
 #check if any new notice numbers
 if len(new_notice_numbers)==0:
-    print("no new notice_numbers")
+    print("   0 new notice_numbers added")
+    result = prev_df #since no new entries, the result is just the old dataframe. 
     
 else:
-    print("{} new notice_numbers found".format(len(new_notice_numbers)))
+    print("   {} new notice_numbers found".format(len(new_notice_numbers)))
     print(new_notice_numbers)
     
     #we'll only work with these
@@ -49,7 +94,7 @@ else:
         raise ValueError("Not all policy numbers are unique")
         
     else: #Get details per notice_number
-        print("Retrieving penalty info...")
+        print("4. Get penalty info...")
         #empty list to collect each row as a dictionary
         penalties = []
 
@@ -67,13 +112,38 @@ else:
         utils.cleanup_dataframe(penalties_df)
         notice_df = utils.join_dataframes(penalties_df, notice_df)
         notice_df = utils.add_timestamp(notice_df)
-        
-        # Concatenate (append) df2 below df1
-        result = pd.concat([prev_df, notice_df], ignore_index=True)  # Reset index for clean numbering
 
-        #overwrite dataset
-        print("Updating Dataset...")
-        result.to_csv("nsw_food_auth_name_and_shame.csv", index=False)
-        print("Complete; dataset Shape:{}".format(result.shape))
+print(len(removed_notice_numbers)) #dev
+print(len(new_notice_numbers)) #dev
+
+if len(removed_notice_numbers)>0 or len(new_notice_numbers) >0:
+    #overwrite dataset
+    print("There have been changes to the dataset")
+    print("5. Begin Upload back to S3...")
+    print("   Dataset Shape:{}".format(result.shape))
+
+    # Reuse the bucket name but change object key
+    #object_key = 'test.csv'
+    print("This will write {} to bucket:{}".format(object_key, bucket_name))
+    
+    # Concatenate (append) df2 below df1
+    result = pd.concat([prev_df, notice_df], ignore_index=True)  # Reset index for clean numbering
+    result.sort_values(by='published_date', inplace=True)
+
+    #result.to_csv('dataset.csv', index=False)
+    #######
+    # Convert DataFrame to CSV string
+    csv_buffer = io.StringIO()
+    result.to_csv(csv_buffer, index=False)  # Set index=False if you don't want row numbers
+
+    # Upload to S3
+    s3.put_object(
+        Bucket=bucket_name, 
+        Key=object_key, 
+        Body=csv_buffer.getvalue()
+    )
+
+    print("object pushed to S3")
+#####
 
 print("END SCRIPT.")
